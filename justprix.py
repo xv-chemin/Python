b43cdd30ba5c2db942fbede70b2924117d660271
Xavier <xavier.chemin@viacesi.fr>
2020-06-04 22:26:40 +0200

    api ok

--- justprix.py
+++ justprix.py
@@ -2,33 +2,66 @@
 # -*- coding: utf-8- -*-
 
 import flask
-from random import randint
+import os
+import requests
+import json
+from random import randrange
+from flask import Flask, render_template, request
 
-nbAleatoire = randint(0, 100)
-print(nbAleatoire)
+app = Flask(__name__)
 
-nbEntre = input("Quel nombre choisi-tu ? ")
-nbEntre = int(nbEntre)
-a = 0
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        url = "https://api.cdiscount.com/OpenApi/json/Search"
+        params = {
+            "ApiKey": "38e82e08-ec23-4b46-ac36-3e3f903a264d",
+            "SearchRequest": {
+                "Keyword": "tv",
+                "Pagination": {
+                    "ItemsPerPage": 5,
+                    "PageNumber": 5
+                },
+                "Filters": {
+                    "Price": {
+                        "Min": 0,
+                        "Max": 0
+                    },
+                    "Navigation": "",
+                    "IncludeMarketPlace": "false",
+                    "Condition": None
+                }
+            }
+        }
 
-essaye = 6
+        response = requests.post(url, data=json.dumps(params))
+        pr = json.loads(response.text)
+        objet = pr['Products'][randrange(0, len(pr['Products']))]
+        image = objet['MainImageUrl']
+        price = round(float(objet['BestOffer']['SalePrice']), 2)
+        name = objet['Name']
+        print(image, price, name)
 
+    if request.method == 'POST':
+        price = price
+        name = name
+        image = image
+        essaye = request.form['prix']
+        print(essaye)
+        if essaye < price:
+            data
+            resultat = "C'est plus ! essaye encore"
+        elif essaye > price:
+            resultat = "C'est moins ! essaye encore"
+        elif essaye == price:
+            resultat = "Bravo"
+        return render_template('home.html', resultat=resultat, price=price, name=name, image=image, lastTry=essaye)
+    else:
+        return render_template('home.html', resultat="", price=price, name=name, image=image, lastTry="")
 
-while nbEntre != nbAleatoire and a < 6 and essaye != 1 :
-    if nbEntre < nbAleatoire :
-        print("c'est plus")
-        essaye -= 1
-        print("Plus que "+ str(essaye) +" coups")
-        nbEntre = int(input("Essaye encore :"))
-    elif nbEntre > nbAleatoire :
-        print("c'est moins")
-        essaye -= 1
-        print("Plus que "+ str(essaye) +" coups")
-        nbEntre = int(input("Essaye encore :"))
-    if nbEntre == nbAleatoire :
-        print("Bravo")
-    if essaye == 1 :
-        print("Perdu")
+if __name__ == '__main__':
+    app.run(debug= True)
+
 
 
 
